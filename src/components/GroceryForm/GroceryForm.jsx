495b176cb06840a6249a4442ee848fe498e50409
Sabrina Ciaciura <ciaci0028@gmail.com>
2022-01-13 10:34:50 -0600

    added form components with inputs and lable

--- src/components/GroceryForm/GroceryForm.jsx
+++ src/components/GroceryForm/GroceryForm.jsx
@@ -2,6 +2,32 @@ function GroceryForm () {
     return (
         <>
             <h1>Add Groceries</h1>
+            <form>
+                <label>
+                    Item
+                </label>
+                <input
+                    type="text"
+                    placeholder="Item"
+                ></input>
+                <label>
+                    Quantity
+                </label>
+                <input
+                    type="text"
+                    placeholder="Quantity"
+                ></input>
+                <label>
+                    Units
+                </label>
+                <input
+                    type="text"
+                    placeholder="Units"
+                ></input>
+                <button>
+                    Submit
+                </button>
+            </form>
         </>
     )
 };
