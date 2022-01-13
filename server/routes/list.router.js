a7828e912d02b7b208d25077108c182dab933970
Kayode Adebanjo <kay.adebanjo@gmail.com>
2022-01-13 11:07:26 -0600

    post endpoint done

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,20 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+// POST students
+router.post('/', (req, res) => {
+    const newShoppingList = req.body.list;
+    const sqlText = `INSERT INTO groceries (list) VALUES ($1)`;
+
+    pool.query(sqlText, [newShoppingList])
+        .then((result) => {
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500);
+        });
+});
+
+
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'react';
 
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+
+    axios.post('/list', newShoppingList)
+    .then( (response) => {
+      console.log('Response:', response);
+      fetchShoppingList();
+    })
+    .catch(function (error) {
+      console.log('Error on add:', error);
+    });
     return (
         <div className="App">
             <Header />
